ee9f9bedd4a464b045e30da546dd61b25b36aa04
Danylo Bezghin <danylobezgin@gmail.com>
2026-07-12 19:37:28 +0300

    feat: check mime type of thumbnail, replace base64 image in db with writing to the disk

--- src/api/thumbnails.ts
+++ src/api/thumbnails.ts
@@ -1,17 +1,10 @@
-import { Buffer } from 'node:buffer';
 import { getBearerToken, validateJWT } from "../auth";
+import { getAssetDiskPath, getAssetURL, mediaTypeToExt } from "./assets";
 import { respondWithJSON } from "./json";
 import { getVideo, updateVideo } from "../db/videos";
 import type { ApiConfig } from "../config";
 import type { BunRequest } from "bun";
 import { BadRequestError, NotFoundError, UserForbiddenError } from "./errors";
-
-type Thumbnail = {
-  data: ArrayBuffer;
-  mediaType: string;
-};
-
-const MAX_UPLOAD_SIZE = 10 << 20; // 10mb
 
 export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const { videoId } = req.params as { videoId?: string };
@@ -22,35 +15,46 @@ export async function handlerUploadThumbnail(cfg: ApiConfig, req: BunRequest) {
   const token = getBearerToken(req.headers);
   const userID = validateJWT(token, cfg.jwtSecret);
 
-  console.log("uploading thumbnail for video", videoId, "by user", userID);
-
-  const formData = await req.formData();
-  const imageData = await formData.get('thumbnail');
-  if (!(imageData instanceof File)) {
-    throw new BadRequestError("Wrong thumbnail data type");
-  }
-
-  if (imageData.size > MAX_UPLOAD_SIZE) {
-    throw new BadRequestError("File should be < 10mb");
-  }
-
-  const mediaType = imageData.type;
-  const buffer = await imageData.arrayBuffer();
-  const buf = Buffer.from(buffer)
-  const imageStr = buf.toString('base64');
-  const dataUrl = `data:${mediaType};base64,${imageStr}`
   const video = getVideo(cfg.db, videoId);
-
   if (!video) {
     throw new NotFoundError("Couldn't find video");
   }
-
   if (video.userID !== userID) {
-    throw new UserForbiddenError('Forbidden');
+    throw new UserForbiddenError("Not authorized to update this video");
   }
 
-  video.thumbnailURL = dataUrl
+  const formData = await req.formData();
+  const file = formData.get("thumbnail");
+  if (!(file instanceof File)) {
+    throw new BadRequestError("Thumbnail file missing");
+  }
 
+  const MAX_UPLOAD_SIZE = 10 << 20;
+
+  if (file.size > MAX_UPLOAD_SIZE) {
+    throw new BadRequestError(
+      `Thumbnail file exceeds the maximum allowed size of 10MB`,
+    );
+  }
+
+  const mediaType = file.type;
+  console.log('mediatype ', mediaType)
+  if (!mediaType) {
+    throw new BadRequestError("Missing Content-Type for thumbnail");
+  }
+
+  if (mediaType !== "image/jpeg" && mediaType !== "image/png") {
+    throw new BadRequestError("jpeg or png only");
+  }
+
+  const ext = mediaTypeToExt(mediaType);
+  const filename = `${videoId}${ext}`;
+
+  const assetDiskPath = getAssetDiskPath(cfg, filename);
+  await Bun.write(assetDiskPath, file);
+
+  const urlPath = getAssetURL(cfg, filename);
+  video.thumbnailURL = urlPath;
   updateVideo(cfg.db, video);
 
   return respondWithJSON(200, video);
